a811f4efba84b5e08552a50a1927b0cb829a1a90
Ethan Kennedy <etkennedy07@gmail.com>
2025-10-30 16:35:20 -0600

    These are my edits for Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your percentage grade here: ");
+        String letterGrade = "";
+        String writtenPercentage = Console.ReadLine();
+        int percentage = int.Parse(writtenPercentage.Trim());
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (percentage < 60)
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine("You got a " + letterGrade + " in the class");
+        if (percentage > 70)
+        {
+            Console.WriteLine("You passed the class congradulations!");
+        } else
+        {
+            Console.WriteLine("You did not pass the class, good luck next time!");
+        }
     }
 }
