7eb80302678f30da6c2dc1743532d950dd2906e0
Ethan Kennedy <etkennedy07@gmail.com>
2025-11-04 10:50:44 -0700

    Class content and prep3

--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,21 @@
 {
     static void Main()
     {
-        Console.WriteLine("Boujour tout le monde");
+        // Console.WriteLine("Boujour tout le monde");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 1234.343;
+
+        // mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 123124.1245;
+        // venus.DisplayPlanetInformation();
+
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem._solarSystem.Add(mercury);
+        solarSystem._solarSystem.Add(venus);
+        solarSystem.DisplaySolarSystem();
     }
 }
