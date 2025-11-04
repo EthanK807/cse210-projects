8e3553d2feb9277b18515a6fd4e8a432ca6509eb
Ethan Kennedy <etkennedy07@gmail.com>
2025-11-04 09:45:17 -0700

    Project for solar system

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        // for (int i = -10000; i < 10000; i += 100)
+        // {
+        //     Console.WriteLine("" + i);
+        // }
+        List<int> numbers = new List<int>();
+        numbers.Add(10);
+        numbers.Add(20);
+        numbers.Add(30);
+        numbers.Add(40);
+
+        
+
+    
     }
+    
 }
