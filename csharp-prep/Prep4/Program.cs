a581c0dbd1c33764b52190906bf7b1eb3a36dc12
Ethan Kennedy <etkennedy07@gmail.com>
2025-11-04 14:39:31 -0700

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list = new List<int>();
+        while (true)
+        {
+            int sum = 0;
+            double average = 0;
+            int largestNumber = int.MinValue;
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            while (true)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value == 0)
+                {
+                    foreach (int i in list)
+                    {
+                        sum += i;
+                        if (i > largestNumber)
+                        {
+                            largestNumber = i;
+                        }
+                    }
+                    average = sum / list.Count;
+                    Console.WriteLine($"The sum is: {sum}\nThe average is: {average}\nThe largest number is: {largestNumber}");
+                    break;
+                }
+                else
+                {
+                    list.Add(value);
+                }
+            }
+        }
     }
 }
