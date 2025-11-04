21603189629211cd655d4a85b303b98ec024dbf5
Ethan Kennedy <etkennedy07@gmail.com>
2025-11-04 14:01:25 -0700

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Formats.Asn1;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+
+        while (true)
+        {
+        int randomValue = random.Next(0, 100);
+            do
+            {
+                Console.Write("What is your guess?");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == randomValue)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                else if (answer < randomValue)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (answer > randomValue)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (true);
+            Console.WriteLine("Would you like to play again?");
+            if (Console.ReadLine().ToUpper().Equals("YES"))
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing!");
+                break;
+            }
+        }
     }
 }
