ebd29de1433d7ef3c5d5d0473d750c7b1223987c
Ethan Kennedy <etkennedy07@gmail.com>
2025-11-06 15:13:35 -0700

    Abstraction Project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Menu journalMenu = new Menu();
+
+        int userSelection;
+
+        bool done = false;
+
+        while (!done)
+        {
+
+            userSelection = journalMenu.ProcessMenu();
+
+            switch (userSelection)
+            {
+                case 1:
+                    // Create a new Entry object
+                    // Call create on that object
+                    // Add the entry to the journal
+                    break;
+                case 2:
+                    // Call journal.Display()
+                    break;
+                case 3:
+                    // Save to a file
+                    break;
+                case 4:
+                    // Read from a file
+                    break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        }
+
     }
 }
