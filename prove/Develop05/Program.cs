e18c615124a961d285668d31184b679e98e4114b
Ethan Kennedy <etkennedy07@gmail.com>
2025-12-07 22:48:50 -0700

    Not fixed yet but getting there

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,20 +1,51 @@
 using System;
 
 class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        GoalManager manager = new GoalManager();
+
+        Boolean running = true;
+        while (running)
         {
-            try 
+            Console.WriteLine("\n--- Eternal Quest Menu ---");
+            manager.DisplayPlayerInfo();
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
             {
-                GoalManager manager = new GoalManager();
-                manager.Start();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("--------------------------------------------------");
-                Console.WriteLine($"CRITICAL ERROR: {ex.Message}");
-                Console.WriteLine("--------------------------------------------------");
-                Console.WriteLine(ex.StackTrace);
+                case "1":
+                    manager.CreateGoal();
+                    break;
+                case "2":
+                    manager.ListGoalDetails();
+                    break;
+                case "3":
+                    manager.SaveGoals();
+                    break;
+                case "4":
+                    manager.LoadGoals();
+                    break;
+                case "5":
+                    RecordEvent();
+                    break;
+                case "6":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    break;
             }
         }
     }
+    }
+}
